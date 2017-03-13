a26a39be14affbd8496c25f4be41eeff6b49e46b
pedro <pedro_abreu95@hotmail.com>
2017-03-13 09:22:46 +0000

    fixed

--- sequential.c
+++ sequential.c
@@ -11,26 +11,27 @@ typedef struct _cell{
 
 int main(int argc, char* argv[]){
 	char* fileName = argv[1];
-	int number_generations = atoi(argv[2]);
 	FILE* fp = fopen(fileName, "r");
+
+	int number_generations = atoi(argv[2]);
+
 	int first = 0;
 	int cube_size;
-	char line[6]; //Only need 6 fo x y z
+	char line[7]; //Only need 7 fo x y z
+	int x, y, z;
+
 	while(fgets(line, sizeof(line), fp)){ //fgets doesn't strip \n
 		if(!first){
 			sscanf(line, "%d\n", &cube_size);
 			first = 1;
+		}else{
+			sscanf(line, "%d %d %d\n", &x, &y, &z);
+			printf("%d %d %d\n", x, y, z);
 		}
-		printf("%s\n", line);
+		
 
 
 	}
-
-
-
-
-
-
 
 
 	exit(0);
