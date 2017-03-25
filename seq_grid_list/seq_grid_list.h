e28a8281503e51ea1d9d41cdc74fe59407889836
jsbruglie <joao.borrego64@gmail.com>
2017-03-25 14:52:51 +0000

    Working version of seq_grid_list. Fixed bug in 3d swap

--- seq_grid_list/seq_grid_list.h
+++ seq_grid_list/seq_grid_list.h
@@ -13,7 +13,7 @@
 
 typedef unsigned char bool;
 
-/* @brief Notifies the neighbours of (x,y,z) of its aliveness and adds them to list
+/** @brief Notifies the neighbours of (x,y,z) of its aliveness and adds them to list
  *
  *  @param graph The graph representation
  *  @param cube_size The size of the side of the cube that represents the 3D space
@@ -24,38 +24,41 @@ typedef unsigned char bool;
  */
 void visitNeighbours(GraphNode*** graph, int cube_size, List* list, coordinate x, coordinate y, coordinate z);
 
-/* @brief Initializes the graph representation structure
+/** @brief Initializes the graph representation structure
  *  
- * @param size The size of the side of the cube that represents the 3D space
+ *  @param size The size of the side of the cube that represents the 3D space
  */
 GraphNode*** initGraph(int size);
 
-/* @brief Frees the graph representation from memory
+/** @brief Frees the graph representation from memory
  *  
- * @param size The size of the side of the cube that represents the 3D space
+ *  @param size The size of the side of the cube that represents the 3D space
  */
 void freeGraph(GraphNode*** graph, int size);
 
-/* @brief Prints the graph, and sorts each of the lists
+/** @brief Prints the graph, and sorts each of the lists
  *
- * SHOULD NOT BE CALLED DURING THE CALCULATION OF A GENERATION:
- * SORTING WILL BREAK POINTER LOGIC
+ *  SHOULD NOT BE CALLED DURING THE CALCULATION OF A GENERATION:
+ *  SORTING WILL BREAK POINTER LOGIC
  *
- * @param graph The graph representation    
- * @param size The size of the side of the cube that represents the 3D space
+ *  @param graph The graph representation    
+ *  @param size The size of the side of the cube that represents the 3D space
  */
 void printAndSortActive(GraphNode*** graph, int cube_size);
 
-/* @brief Parse command line arguments
+/** @brief Parse command line arguments
  *
- * @param argc Number of arguments
- * @param argv Argument string
- * @param file Output   
+ *  @param argc Number of arguments
+ *  @param argv Argument string
+ *  @param file Output   
  */
 void parseArgs(int argc, char* argv[], char** file, int* generations);
 
-/* @brief 
+/** @brief Parse input file contents 
  *
+ *  @param file Filename string
+ *  @param list List for keeping track of live cells and neighbours
+ *  @param cube_size The size of the side of the cube that represents the 3D space
  */
 GraphNode*** parseFile(char* file, List* list, int* cube_size);    
 
